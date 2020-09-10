7e3a5c41ee08ec1cbe86e3b6fad47598631707f0
codecakes <akulmat@protonmail.com>
2020-09-10 18:07:44 +0530

    constants Takes all env from secrets. changed in node wf gh build. package.json: updated to build-dev for local testing. runs startup script to create env file

--- src/firebase/firebase-core.js
+++ src/firebase/firebase-core.js
@@ -1,16 +1,19 @@
 /* jshint esversion: 6 */
 
+import CONSTANTS from "./constants";
+
+
 // Your web app's Firebase configuration
 /** @type {Map<string, string} */
 const firebaseConfig = {
-    apiKey: "AIzaSyDe7gyhhCgE0qn_VzEBIS6xaztC4Y670pI",
-    authDomain: "biz-leads-1509126536502.firebaseapp.com",
-    databaseURL: "https://biz-leads-1509126536502.firebaseio.com",
-    projectId: "biz-leads-1509126536502",
-    storageBucket: "biz-leads-1509126536502.appspot.com",
-    messagingSenderId: "604540107625",
-    appId: "1:604540107625:web:6913876c7bd828ae225bf8",
-    measurementId: "G-ZR8QKBRSMX"
+    apiKey: CONSTANTS.FIREBASE_API_KEY,
+    authDomain: CONSTANTS.FIREBASE_AUTH_DOMAIN,
+    databaseURL: CONSTANTS.FIREBASE_DB_URL,
+    projectId: CONSTANTS.FIREBASE_PROJECT_ID,
+    storageBucket: CONSTANTS.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: CONSTANTS.FIREBASE_MSG_SENDER_ID,
+    appId: CONSTANTS.FIREBASE_APP_ID,
+    measurementId: CONSTANTS.FIREBASE_MEASUREMENT_ID,
 };
 
 
