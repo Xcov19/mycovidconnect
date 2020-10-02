da40cfa4b378a68574846a24e9e7d0eb79b66606
Jason Wei <jason.wei.1106@gmail.com>
2020-10-02 13:04:06 -0700

    MYC-73 MYC-113> #comment add cookiebot lib

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Logo from "./Logo";
+import Cookie from "./Cookie";
+
+
 
 const Header = () => {
   return (
     <header>
       <div className="headerin">
-        <Logo />
+      <Logo />
+      </div>
+      <div>
+      <Cookie/>
       </div>
     </header>
   );
 };
+
+
 
 export default Header;
