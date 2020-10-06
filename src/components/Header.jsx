0efc6520d72b8718da716e1f92f3c090a05ac7b4
Andy Nixon <andielane@yahoo.com>
2020-10-06 09:17:07 -0700

    Importing Session Expiry Component into Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "./Logo";
+import SessionExpiry from "./SessionExpiry.jsx"
 
 const Header = () => {
   return (
     <header>
       <div className="headerin">
         <Logo />
+        <SessionExpiry/>
       </div>
     </header>
   );
