02f311911d9173fb8a1f17b5caae94b12453d29b
Jason Wei <jason.wei.1106@gmail.com>
2020-10-08 15:49:04 -0700

    MYC-113 MYC-73 #comment remove cookiebot npm and adding only script tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import Logo from "./Logo";
-import Cookie from "./Cookie";
-
-
 
 const Header = () => {
   return (
     <header>
       <div className="headerin">
-      <Logo />
+        <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
+        <Logo />
       </div>
-      <div>
-      <Cookie/>
-      </div>
+      <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="54349014-3934-489b-a905-584bc83272b2" data-blockingmode="auto" type="text/javascript"></script>
     </header>
   );
 };
-
-
 
 export default Header;
