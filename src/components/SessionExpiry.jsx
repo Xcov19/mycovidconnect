a0f9b7b62a88da17786beb32ad115e57d9b13838
Andy Nixon <andielane@yahoo.com>
2020-10-08 10:16:35 -0700

    Adding a dismiss function to close Modal without resetting timer

--- src/components/SessionExpiry.jsx
+++ src/components/SessionExpiry.jsx
@@ -4,19 +4,24 @@ const SessionExpiry = () => {
   const [timeLeft, setTimeLeft] = useState(600);
   const [isReset, setIsReset] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [isDismissed, setIsDismissed] = useState(false)
 
   const resetTime = e => {
     setIsReset(!isReset)
   }
 
+  const dismiss = () => {
+    setIsDismissed(true)
+  }
+
   const displayTime = (timeLeft) => {
     const hh = Math.floor(timeLeft/60);
-    const mm = `${Math.floor(timeLeft % 60)}`.padStart(2, "0");
-    
+    const mm = `${Math.floor(timeLeft % 60)}`.padStart(2, "0");   
     return `${hh}:${mm}`;
   }
 
   const autoLogOut = () => {
+    console.log(timeLeft)
     const countDown = () => {
       if(isReset){
         setTimeLeft(600)
@@ -30,6 +35,7 @@ const SessionExpiry = () => {
         countDown();
       },100)
     } else if(isLoggedIn && timeLeft <= 0){
+      setIsDismissed(false)
       setIsLoggedIn(false)
       setTimeLeft(0)
     }
@@ -37,26 +43,30 @@ const SessionExpiry = () => {
 
   useEffect(autoLogOut);
 
-  if(timeLeft < 60 && isLoggedIn){
-    return (
-      <div className="sessionExpiry">
-        <div className="topBar"><span className="topBarButton" onClick={resetTime}>&times;</span></div>
-        <div className="warning">Please note that your session is about to expire in {displayTime(timeLeft)}</div>
-        <button className="sessionExpiryButton  " onClick={() => setIsReset(!isReset)}>Continue session</button>
-      </div>
-  );
+  if(!isDismissed){
+    if(timeLeft < 60 && isLoggedIn){
+      return (
+        <div className="sessionExpiry">
+          <div className="topBar"><span className="topBarButton" onClick={dismiss}>&times;</span></div>
+          <div className="warning">Please note that your session is about to expire in {displayTime(timeLeft)}</div>
+          <button className="sessionExpiryButton  " onClick={() => setIsReset(!isReset)}>Continue session</button>
+        </div>
+    );
 
-  } else if (!isLoggedIn){
-    return (
-      <div className="sessionExpiry">
-      <div className="topBar"></div>
-      <div className="warning">You have been Logged Out</div>
-    </div>
-    )
+    } else if (!isLoggedIn){
+      return (
+        <div className="sessionExpiry">
+          <div className="topBar"><span className="topBarButton" onClick={dismiss}>&times;</span></div>
+          <div className="warning">You have been Logged Out</div>
+        </div>
+      )
+    } else {
+      return (
+        null
+      )
+    }
   } else {
-    return (
-      null
-    )
+    return null
   }
 };
 
