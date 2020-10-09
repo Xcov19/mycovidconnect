84200ab76024d04bbb3f3e97fee8e32ac80ece19
Jason Wei <jason.wei.1106@gmail.com>
2020-10-09 11:30:06 -0700

    MYC-73 MYC-113 #comment Adding scripts to index.html and remove from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,8 @@ const Header = () => {
   return (
     <header>
       <div className="headerin">
-        <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
         <Logo />
       </div>
-      <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="54349014-3934-489b-a905-584bc83272b2" data-blockingmode="auto" type="text/javascript"></script>
     </header>
   );
 };
