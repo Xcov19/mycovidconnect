95abf55422e03b715abba66a51e5372e5ab91619
nivedhasamy <nivedhasamy@gmail.com>
2020-10-12 12:19:28 +0530

    uber integration, refactor map component, jsdocs for search and map component

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
+const UBER_CLIENT_ID = process.env.REACT_APP_UBER_CLIENT_ID;
 const alternativeCityNamesLookup = {
   "bangalore urban": "bangalore",
   "bengaluru": "bangalore",
 };
 
-export { GOOGLE_MAPS_API_KEY, alternativeCityNamesLookup };
+export { GOOGLE_MAPS_API_KEY,UBER_CLIENT_ID, alternativeCityNamesLookup };
