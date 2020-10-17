495c8a51f35aa850b9ab0e394fd59eeb18a34cb3
ksr89 <kuljeetginyu@gmail.com>
2020-10-17 15:15:25 +0530

    remove link from header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,7 @@ const Header = () => {
   return (
     <header>
       <div className="headerin">
-        <Link to="/">
-          <Logo />
-        </Link>
+        <Logo />
       </div>
     </header>
   );
