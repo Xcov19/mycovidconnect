a752388bb060b6dcd2dab181b0f260dc7adaa604
ksr89 <kuljeetginyu@gmail.com>
2020-10-20 23:33:23 +0530

    MYC-113 #comment add popup

--- src/components/CookieDeclaration.jsx
+++ src/components/CookieDeclaration.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 
 const CookieDeclaration = ({ show }) => {
-  // if (show) {
-    return <React.Fragment>
-      <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
-    </React.Fragment>;
-  // }
-  // else {
-  //   return (null)
-  // }
+  if (show) {
+    // return <React.Fragment>
+    //   <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
+    // </React.Fragment>;
+    return (
+      <>
+        <div style={{
+          width: "500px",
+          height: "500px",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          margin: "auto",
+          backgroundColor: "red"
+        }}
+        >
+          CookieDeclaration Popup
+        </div>
+        <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
+      </>
+    )
+  }
+  else {
+    return (null)
+  }
 };
 
 export default CookieDeclaration;
