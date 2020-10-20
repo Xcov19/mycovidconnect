966d94053ee5ec6e3a0d0ae3e7c0f06dadef3eff
ksr89 <kuljeetginyu@gmail.com>
2020-10-21 00:19:13 +0530

    MYC-113 #comment add popup for cookis data

--- src/components/CookieDeclaration.jsx
+++ src/components/CookieDeclaration.jsx
@@ -4,21 +4,35 @@ const cookieDeclaration = ({ show }) => {
   if (show) {
 
     const script = document.createElement('script');
+
     script.src = "https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js";
     script.async = true;
-    document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    }
+    document.getElementById("cookieData").appendChild(script);
 
-    return <React.Fragment>
-      <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
-    </React.Fragment>;
+
+
+    // return <React.Fragment>
+    //   <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
+    // </React.Fragment>;
   }
   else {
-    return (null)
+    // return (null)
   }
+
+  return (
+    <div style={{
+      width: "500px",
+      height: "500px",
+      left: "0",
+      top: "0",
+      bottom: "0",
+      right: "0",
+      margin: "auto",
+      position: "absolute",
+      backgroundColor: "white"
+    }} id="cookieData"></div>
+  )
 };
 
 export default cookieDeclaration;
