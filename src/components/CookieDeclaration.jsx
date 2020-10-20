4207361e394cb88566d66d5fdce7e3584e4f48b9
ksr89 <kuljeetginyu@gmail.com>
2020-10-20 23:20:31 +0530

    MYC-113 #comment remove constion to show CookieDeclaration

--- src/components/CookieDeclaration.jsx
+++ src/components/CookieDeclaration.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const cookieDeclaration = ({show}) => {
-  if(show){
+const CookieDeclaration = ({ show }) => {
+  // if (show) {
     return <React.Fragment>
-            <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
-        </React.Fragment>;
-  }
-  else{
-    return(null)
-  }
+      <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
+    </React.Fragment>;
+  // }
+  // else {
+  //   return (null)
+  // }
 };
 
-export default cookieDeclaration;
+export default CookieDeclaration;
--- src/components/MainHOC.jsx
+++ src/components/MainHOC.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Copyrights from './Copyrights';
-import CookieDeclaration from "../components/CookieDeclaration";
+import CookieDeclaration from "./CookieDeclaration";
 
 
 const MaindHOC = (WrappedComponent) => {
