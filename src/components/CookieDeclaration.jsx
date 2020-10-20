a468c5f6ec7b7743f36a2e43fb3ae07c6f5abfa4
ksr89 <kuljeetginyu@gmail.com>
2020-10-21 00:13:09 +0530

    working on cookibot dialog

--- src/components/CookieDeclaration.jsx
+++ src/components/CookieDeclaration.jsx
@@ -1,33 +1,24 @@
 import React from "react";
 
-const CookieDeclaration = ({ show }) => {
+const cookieDeclaration = ({ show }) => {
   if (show) {
-    // return <React.Fragment>
-    //   <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
-    // </React.Fragment>;
-    return (
-      <>
-        <div style={{
-          width: "500px",
-          height: "500px",
-          top: "0",
-          left: "0",
-          bottom: "0",
-          right: "0",
-          margin: "auto",
-          backgroundColor: "red",
-          position: "absolute"
-        }}
-        >
-          CookieDeclaration Popup
-        </div>
-        <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
-      </>
-    )
+
+    const script = document.createElement('script');
+    script.src = "https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+
+    return <React.Fragment>
+      <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
+    </React.Fragment>;
   }
   else {
     return (null)
   }
 };
 
-export default CookieDeclaration;
+export default cookieDeclaration;
