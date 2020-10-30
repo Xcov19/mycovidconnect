80c7ab31f196a69d37994c7471ee3e82eb75c52b
nivedhasamy <nivedhasamy@gmail.com>
2020-10-30 18:32:54 +0530

    cookie pop up toggle and styling fixes

--- src/components/CookieDeclaration.jsx
+++ src/components/CookieDeclaration.jsx
@@ -1,44 +1,39 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
-const cookieDeclaration = ({ show,handleclose }) => {
-  if (show) {
+const CookieDeclaration = ({ show, handleclose }) => {
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.src =
+			'https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js';
+		script.async = true;
+		document.getElementById('cookieData').appendChild(script);
+	}, []);
 
-    const script = document.createElement('script');
-
-    script.src = "https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js";
-    script.async = true;
-
-    document.getElementById("cookieData").appendChild(script);
-
-    
-
-    // return <React.Fragment>
-    //   <script id="CookieDeclaration" src="https://consent.cookiebot.com/54349014-3934-489b-a905-584bc83272b2/cd.js" type="text/javascript" async></script>
-    // </React.Fragment>;
-  }
-  else {
-    // return (null)
-  }
-
-  return (
-    
-    <div style={{
-      width: show ? "500px" : "0",
-      height: show ? "500px" : "0",
-      left: "0",
-      top: "0",
-      bottom: "0",
-      right: "0",
-      margin: "auto",
-      position: "absolute",
-      backgroundColor: "white",
-      overflow: "auto"
-    }} id="cookieData">
-
-      <button className = "closebtn" onClick={handleclose}>X</button>
-    </div>
-  
-  )
+	return (
+		<div
+			style={{
+				width: '550px',
+				height: '450px',
+				left: '0',
+				top: '0',
+				bottom: '0',
+				right: '0',
+				margin: 'auto',
+				position: 'absolute',
+				backgroundColor: 'white',
+				overflow: 'auto',
+			}}
+			id="cookieData"
+		>
+			<button className="cookie-close" onClick={handleclose}>
+				X
+			</button>
+			<div className="cookie-title">
+				<img src="/cookie-icon96.png" alt="cookie-icon" />
+				<h3>Cookies & Privacy Policy</h3>
+			</div>
+		</div>
+	);
 };
 
-export default cookieDeclaration;
+export default CookieDeclaration;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,110 +1,103 @@
-import React,{useState,useEffect} from "react";
-import CookieDeclaration from "../components/CookieDeclaration";
-import {COOKIEBOT_SRC} from "../constants";
-
+import React, { useState, useEffect } from 'react';
+import CookieDeclaration from '../components/CookieDeclaration';
+import { COOKIEBOT_SRC } from '../constants';
 
 const Footer = (props) => {
-  const [show,setShow] = useState(false);
-  
-  useEffect(() => {
-    var src=COOKIEBOT_SRC
-    console.log(`${src}`);
-    var cd = document.querySelector('script[src="' + src + '"]');
-    if(cd != null){
-    }else{
-      setShow(false);
-    }
-  },[show]);
+	const [show, setShow] = useState(false);
 
-  const showModal = (e) => {
-    e.preventDefault();
-    setShow(true);
-    return false;
-  }
+	useEffect(() => {
+		var src = COOKIEBOT_SRC;
+		var cd = document.querySelector('script[src="' + src + '"]');
+		// if (cd != null) {
+		// } else {
+		// 	setShow(false);
+		// }
+	}, [show]);
 
-  const hidemodal = () => {
-    setShow(false)
-  }
-  return (
-    <>
-      <footer>
-        <div className="footerin">
-          <div className="w40">
-            <h3>Join Our Community</h3>
-            <a
-              target="_blank"
-              rel='noreferrer noopener'
-              href="https://forum.mycovidconnect.com/"
-              title="Join Now"
-            >
-              <button>Join Now</button>
-            </a>
-          </div>
-          <div className="w30">
-            <div className="links">
-              <a
-                target="_blank"
-                rel='noreferrer noopener'
-                href="https://forum.mycovidconnect.com/d/2-about-xcov19-rapid-action-patient-dispatch-solution"
-              >
-                About XCOV19
-              </a>
-              <a
-                href="mailto:reachout@mycovidconnect.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Contact Us
-              </a>
-              <a
-                target="_blank"
-                rel='noreferrer' 
-                href="/" onClick = {showModal}>Privacy Policy</a>
-            </div>
-            <div className="media-links">
-              <div className="first-row">
-                <a href="https://hacktoberfest.cloud.mattermost.com/main/channels/mlh-4616">
-                  <img
-                    src={
-                      'https://d2vgampz89jm7o.cloudfront.net/_Sponsors+Light+Boxed+2.svg'
-                    }
-                    alt="hacktoberfest-sponsors-logo"
-                  />
-                </a>
-                <a href="https://app.mattermore.io/matters/TWF0dGVyOjky/xcov19-rapid-action-patient-dispatch-solution-for-covid-19">
-                  <img
-                    src={
-                      'https://d2vgampz89jm7o.cloudfront.net/mattermore_logo.jpeg'
-                    }
-                    alt="mattermore-logo"
-                    id="square"
-                  />
-                </a>
-              </div>
-              <div className="second-row">
-                <a href="https://helpful.directory/projects/HE-1594886592000">
-                  <img
-                    src={
-                      'https://d2vgampz89jm7o.cloudfront.net/helpful-engineering-logo-1.png'
-                    }
-                    alt="helpful-logo"
-                  />
-                </a>
-                <a href="https://app.jogl.io/project/251/about">
-                  <img
-                    src={'https://d2vgampz89jm7o.cloudfront.net/JOGL_logo.png'}
-                    alt="jogl-logo"
-                    id="square"
-                  />
-                </a>
-              </div>
-              <CookieDeclaration show={show} handleclose ={hidemodal}/>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </>
-  );
+	const showModal = (e) => {
+		setShow(true);
+	};
+
+	const hidemodal = () => {
+		setShow(false);
+	};
+	return (
+		<>
+			<footer>
+				<div className="footerin">
+					<div className="w40">
+						<h3>Join Our Community</h3>
+						<a
+							target="_blank"
+							rel="noreferrer noopener"
+							href="https://forum.mycovidconnect.com/"
+							title="Join Now"
+						>
+							<button>Join Now</button>
+						</a>
+					</div>
+					<div className="w30">
+						<div className="links">
+							<a
+								target="_blank"
+								rel="noreferrer noopener"
+								href="https://forum.mycovidconnect.com/d/2-about-xcov19-rapid-action-patient-dispatch-solution"
+							>
+								About XCOV19
+							</a>
+							<a
+								href="mailto:reachout@mycovidconnect.com"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Contact Us
+							</a>
+							<a rel="noreferrer" href="#" onClick={showModal}>
+								Privacy Policy
+							</a>
+						</div>
+						<div className="media-links">
+							<div className="first-row">
+								<a href="https://hacktoberfest.cloud.mattermost.com/main/channels/mlh-4616">
+									<img
+										src={
+											'https://d2vgampz89jm7o.cloudfront.net/_Sponsors+Light+Boxed+2.svg'
+										}
+										alt="hacktoberfest-sponsors-logo"
+									/>
+								</a>
+								<a href="https://app.mattermore.io/matters/TWF0dGVyOjky/xcov19-rapid-action-patient-dispatch-solution-for-covid-19">
+									<img
+										src={'https://d2vgampz89jm7o.cloudfront.net/mattermore_logo.jpeg'}
+										alt="mattermore-logo"
+										id="square"
+									/>
+								</a>
+							</div>
+							<div className="second-row">
+								<a href="https://helpful.directory/projects/HE-1594886592000">
+									<img
+										src={
+											'https://d2vgampz89jm7o.cloudfront.net/helpful-engineering-logo-1.png'
+										}
+										alt="helpful-logo"
+									/>
+								</a>
+								<a href="https://app.jogl.io/project/251/about">
+									<img
+										src={'https://d2vgampz89jm7o.cloudfront.net/JOGL_logo.png'}
+										alt="jogl-logo"
+										id="square"
+									/>
+								</a>
+							</div>
+							{show && <CookieDeclaration show={show} handleclose={hidemodal} />}
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
 };
 
 export default Footer;
