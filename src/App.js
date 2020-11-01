40140973369689922b73f1fdd85b30c73956414e
nivedhasamy <nivedhasamy@gmail.com>
2020-11-01 16:57:07 +0530

    move tawk and cookiebot scripts to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Home from "./containers/Home";
-import Search from "./containers/Search";
-
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './containers/Home';
+import Search from './containers/Search';
+import {
+	COOKIEBOT_CONSENT_HEAD,
+	COOKIEBOT_CONSENT_HEAD_DATA_CBID,
+	TAWKTO,
+} from './constants/index';
 function App() {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/search/:lat/:lng" component={Search} />
-        </Switch>
-      </Router>
-    </>
-  );
-}
+	useEffect(() => {
+		//import cookiebot script
+		const fragment = document.createDocumentFragment();
+		const script = document.createElement('script');
+		script.src = COOKIEBOT_CONSENT_HEAD;
+		script.async = true;
+		script.setAttribute('data-cbid', COOKIEBOT_CONSENT_HEAD_DATA_CBID);
+		fragment.appendChild(script);
+		document
+			.querySelector('head')
+			.insertBefore(fragment, document.head.firstChild);
 
+		//import tawk script
+		var Tawk_API = Tawk_API || {},
+			Tawk_LoadStart = new Date();
+		const fragmentTawk = document.createDocumentFragment();
+		const scriptTawk = document.createElement('script');
+		scriptTawk.async = true;
+		scriptTawk.src = TAWKTO;
+		scriptTawk.charset = 'UTF-8';
+		scriptTawk.setAttribute('crossorigin', '*');
+		fragmentTawk.appendChild(scriptTawk);
+		document
+			.querySelector('head')
+			.insertBefore(fragmentTawk, document.head.firstChild);
+	}, []);
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/search/:lat/:lng" component={Search} />
+				</Switch>
+			</Router>
+		</>
+	);
+}
 export default App;
