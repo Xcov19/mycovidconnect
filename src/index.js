84150aa7493fbef5b498328cf0122c6b02cc6a48
premalrupnur <premalrupnur2001@gmail.com>
2020-12-17 19:52:58 +0530

    Removes Auth0 Config

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="premal.us.auth0.com"
-    clientId="zk7oKmTD2BtfIjjQJsAQbK4BGUhOZd48"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById("root")
-);
+  <App />, 
+  document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
