5c1519d802d6ca94bcfe2dc87d2c565958727b1e
farhan <farhanzaki@hotmail.com>
2021-11-07 07:04:25 +0500

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 const CACHE_NAME = "version-1";
 const urlsToCache = [ 'index.html', 'offline.html' ];
 
@@ -11,7 +12,7 @@ self.addEventListener('install', (event) => {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
-    )
+    );
 });
 // Listen for requests
 self.addEventListener('fetch', (event) => {
@@ -21,21 +22,20 @@ self.addEventListener('fetch', (event) => {
                 return fetch(event.request) 
                     .catch(() => caches.match('offline.html'))
             })
-    )
+    );  
 });
 // Activate the SW
 self.addEventListener('activate', (event) => {
     const cacheWhitelist = [];
     cacheWhitelist.push(CACHE_NAME);
-
     event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
+         caches.keys().then((cacheNames) => Promise.all(
+         cacheNames.map((cacheName) => {
                 if(!cacheWhitelist.includes(cacheName)) {
                     return caches.delete(cacheName);
                 }
             })
         ))
             
-    )
+    );
 });
