e32f03ebea114bbba63995173ca602422ea491b3
Shehzad <shehzadharoob@gmail.com>
2021-11-11 11:11:44 +0500

    Update

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,9 @@
+
+// eslint-disable-next-line array-callback-return
 const CACHE_NAME = "version-1";
 const urlsToCache = [ 'index.html', 'offline.html' ];
 const SELF = this;
+// eslint-disable-next-line array-callback-return
 SELF.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -10,6 +13,8 @@ SELF.addEventListener('install', (event) => {
             })
     );
 });
+
+// eslint-disable-next-line array-callback-return
 SELF.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
@@ -19,6 +24,8 @@ SELF.addEventListener('fetch', (event) => {
             })
     );  
 });
+
+// eslint-disable-next-line array-callback-return
 SELF.addEventListener('activate', (event) => {
     const cacheWhitelist = [];
     cacheWhitelist.push(CACHE_NAME);
